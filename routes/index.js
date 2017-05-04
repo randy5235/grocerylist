5547fc4fbbda628ad1ecea3443e4cd37229833c2
Randy Wressell <randy5235@hotmail.com>
2017-05-03 22:11:54 -0700

    Update basic routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const router = express.Router();
 router.route('/login')
   .get((req, res) => {
-    res.json('Welcome to login');
+    res.json({ message: 'Welcome to login' });
   });
 
+router.route('/logout')
+  .post((req, res) => {
+    res.json({ message: 'Thank you, come again!' });
+  });
 
 module.exports = router;
