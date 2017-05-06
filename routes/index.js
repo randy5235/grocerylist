d4620b5d745bc528e021b9bbd66f4af6c66ecfbe
Randy Wressell <rwressell@globesherpa.com>
2017-05-06 12:47:51 -0700

    Extract auth into module

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
+const isAuthorized = require('../authentication/auth');
 const express = require('express');
 const list = require('./list');
 const user = require('./user');
 
 const router = express.Router();
-
-function isAuthorized() {
-  return ((req, res, next) => {
-    if (req.headers.authorized !== 'true') {
-      return res.json({ message: 'NOT AUTHORIZED' });
-    }
-    next();
-  });
-}
 
 router.use('/', user);
 router.use(isAuthorized());
