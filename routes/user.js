73bc32017a1b361d10ae008d9577b0123a5f7f4c
Randy Wressell <randy5235@gmail.com>
2017-05-09 22:00:16 -0700

    Pull data from db

--- routes/user.js
+++ routes/user.js
@@ -9,14 +9,15 @@ pool.connect(function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+  client.query('SELECT * FROM users', function(err, result) {
     //call `done(err)` to release the client back to the pool (or destroy it if there is an error) 
     done(err);
  
     if(err) {
       return console.error('error running query', err);
     }
-    console.log(result.rows[0].number);
+    console.log(result.rows[0].username);
+
     //output: 1 
   });
 });
