f0725bef7251232519b8f71c4e85ae1b36a79574
Randy Wressell <randy5235@gmail.com>
2017-05-12 11:37:09 -0700

    remove console.log

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,8 +1,6 @@
 function isAuthorized() {
   return (req, res, next) => {
-    console.log(req.headers);
     if (req.headers.authorized == 'true') {
-      console.log('GOTHERE');
       next();
     } else {
       return res.json({ message: 'NOT AUTHORIZED' });
