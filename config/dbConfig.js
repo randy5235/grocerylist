8578d7b8a14c15ab53211425aec10c603f622654
Randy Wressell <randy5235@gmail.com>
2017-05-16 21:10:54 -0700

    Simple sequelize implementation.. seems decent

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -8,6 +8,7 @@ const dbConfig = {
   port: 5432, // env var: PGPORT
   max: 1, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  url: config.URL,
 };
 
 const sessionSecret = {
