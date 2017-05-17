6a9f790348512da9943ee7ee27115ceaeb490214
Randy Wressell <randy5235@gmail.com>
2017-05-16 21:55:56 -0700

    Update model and simplify

--- controllers/seqUser.js
+++ controllers/seqUser.js
@@ -1,47 +1,29 @@
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//   host: 'localhost',
-//   dialect: 'mysql'|'sqlite'|'postgres'|'mssql',
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   },
-
-//   // SQLite only
-//   storage: 'path/to/database.sqlite'
-// });
-
-// Or you can simply use a connection uri
 const dbConfig = require('../config/dbConfig').dbConfig;
 const Sequelize = require('sequelize');
-// console.log(÷);
+
 const sequelize = new Sequelize(dbConfig.url);
 
-sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
-
-
-  var User = sequelize.define('user', {
+const User = sequelize.define('user', {
+  id: {
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+    field: 'id',
+  },
   username: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    field: 'username',
   },
   password: {
-    type: Sequelize.STRING
-  }
+    type: Sequelize.STRING,
+    field: 'password',
+  },
 });
 
 // force: true will drop the table if it already exists
-User.sync({force: true}).then(function () {
+User.sync({ force: false }).then(() =>
   // Table created
-  return User.create({
-    username: 'John',
-    password: 'test'
-  });
-});
+   User.create({
+     username: 'Randy',
+     password: 'test',
+   }));
