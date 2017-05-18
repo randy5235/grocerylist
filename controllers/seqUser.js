6527553e72d084309cf3388a8b3ab8e419e0452e
Randy Wressell <randy5235@hotmail.com>
2017-05-17 23:20:24 -0700

    Update to async/await

--- controllers/seqUser.js
+++ controllers/seqUser.js
@@ -13,30 +13,24 @@ const User = sequelize.define('user', {
 });
 
 // force: true will drop the table if it already exists
-const userRegister = (req, res, next) => {
-  // console.log(req.body);
-  User.sync({force: true }).then(() =>
-    // Table created
-    User.create({
-      username: req.body.username,
-      password: req.body.password,
-    }).then((user) => {
-      req.user = user;
-      next();
-    })
-  );
-};
+async function userRegister(req, res, next) {
+  let sync = await User.sync({ force: true });
+  let user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.user = user;
+  next();
+}
 
-const getUser = (req, res, next) => {
-  // console.log(req.body);
-  User.sync({force: false }).then(() =>
-    // Table created
-    User.findOne({
-      username: req.body.username,
-    }).then((user) => {
-      req.user = user;
-      next();
-    })
-  );
-};
+async function getUser(req, res, next) {
+  User.sync({ force: false });
+  const user = await User.findOne({
+    username: req.body.username,
+  });
+
+  req.user = user;
+  next();
+}
+
 module.exports = { userRegister, getUser };
