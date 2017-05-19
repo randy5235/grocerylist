e92ee392d68785549ab6fcc4a3be6f9ba6bfca84
Randy Wressell <randy5235@gmail.com>
2017-05-18 21:53:05 -0700

    Add post routes for /list and /list/:list/item

--- routes/list.js
+++ routes/list.js
@@ -8,7 +8,10 @@ router
   .route('/lists')
   .get((req, res) => {
     res.json({ message: 'Get All Lists' });
-  })
+  });
+
+router
+  .route('/list')
   .post(createList, (req, res) => {
     res.json({ listId: req.list.id, title: req.list.title });
   });
@@ -21,6 +24,12 @@ router.route('/list/:list').get((req, res) => {
 router.route('/list/:list/items').get((req, res) => {
   res.json({ message: 'returns all list items for a single list' });
 });
+
+router
+  .route('/list/:list/item')
+  .post((req, res) => {
+    res.json({ message: 'creates a single item associated with a list' });
+  });
 
 router.route('/list/:list/item/:item').get((req, res) => {
   res.json({ message: 'returns a specific item from a specific list' });
