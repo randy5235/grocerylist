99f725c22a98689e5f78553559d0e314df2802e7
Randy Wressell <randy5235@gmail.com>
2017-05-21 13:46:22 -0700

    Add basic bcrypt function might be blocking

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,3 +1,20 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const getUser = require('../controllers/seqUser').getUser;
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+
 function isAuthorized() {
   return (req, res, next) => {
     if (req.isAuthenticated) {
--- controllers/seqUser.js
+++ controllers/seqUser.js
@@ -1,4 +1,5 @@
 const dbConfig = require('../config/dbConfig').dbConfig;
+const bcrypt = require('bcrypt-nodejs');
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(dbConfig.url);
@@ -14,12 +15,23 @@ const User = sequelize.define('user', {
 
 sequelize.sync();
 
+function generateHash(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+}
+
+// checking if password is valid
+// async function comparePassword(password) {
+//     await bcrypt.compare(password, getUser);
+// };
+
 // force: true will drop the table if it already exists
 async function userRegister(req, res, next) {
   // let sync = await User.sync({ force: true });
+  let password = generateHash(req.body.password);
+  console.log(password);
   const user = await User.create({
     username: req.body.username,
-    password: req.body.password
+    password: generateHash(req.body.password)
   });
   req.user = user;
   next();
@@ -33,4 +45,6 @@ async function getUser(req, res, next) {
   next();
 }
 
+// const test = 'test';
+// console.log(generateHash(test));
 module.exports = { userRegister, getUser };
