408844a8f58191a23fba9b4bbed5d417c79bea8e
Randy Wressell <randy5235@gmail.com>
2017-05-29 16:14:38 -0700

    Cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-const isAuthorized = require('./user').isAuthorized;
 const express = require('express');
 const list = require('./list');
 const user = require('./user');
@@ -6,7 +5,6 @@ const user = require('./user');
 const router = express.Router();
 
 router.use('/', user);
-// router.use(isAuthorized);
 router.use('/', list);
 
 module.exports = router;
