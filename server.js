c5fc3ccd4a82ce08a75e0ebfbe28b40976f3ea47
Randy Wressell <randy5235@hotmail.com>
2017-06-03 22:08:59 -0700

    Add morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const { sessionSecret } = require('./config/dbConfig');
 const express = require('express');
+const logger = require('morgan');
 const passport = require('passport');
 const router = require('./routes');
 const session = require('express-session');
@@ -9,6 +10,7 @@ require('./authentication/auth');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(logger('dev'));
 app.use(session(sessionSecret));
 app.use(passport.initialize());
 app.use(passport.session());
