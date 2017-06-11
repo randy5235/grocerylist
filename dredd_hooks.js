86556f788fcd371244007a4dd73dde89208f51f0
Randy Wressell <randy5235@hotmail.com>
2017-06-11 10:59:21 -0700

    Fix all linting issues

--- dredd_hooks.js
+++ dredd_hooks.js
@@ -1,4 +1,6 @@
-'use strict';
+/* eslint import/no-extraneous-dependencies: ["off", {"devDependencies": false}] */
+/* eslint import/no-unresolved: "off" */
+/* eslint no-param-reassign: ["error", { "props": false }] */
 
 const hooks = require('hooks');
 
@@ -6,67 +8,67 @@ const stash = {};
 
 function User() {
   this.username = `test_${Date.now()}`;
-  this.password = `password`;
+  this.password = 'password';
   return { username: this.username, password: this.password };
 }
 
-const getUser = new User;
+const getUser = new User();
 
-hooks.before('users > Register > Create a new user', function (transaction, done) {
-  var requestBody = JSON.parse(transaction.request.body);
+hooks.before('users > Register > Create a new user', (transaction, done) => {
+  // const requestBody = JSON.parse(transaction.request.body);
   transaction.request.body = JSON.stringify(getUser);
   done();
 });
 
-hooks.after('users > Login > Login with an existing user', function (transaction, done) {
-  stash.cookie = transaction.real['headers']['set-cookie'];
+hooks.after('users > Login > Login with an existing user', (transaction, done) => {
+  stash.cookie = transaction.real.headers['set-cookie'];
   done();
 });
 
-hooks.before('lists > lists > Send back a collection of all lists that a user has access to', function (transaction, done) {
-  transaction.request['headers']['Cookie'] = stash.cookie;
+hooks.before('lists > lists > Send back a collection of all lists that a user has access to', (transaction, done) => {
+  transaction.request.headers.Cookie = stash.cookie;
   done();
 });
-hooks.before('lists > list > Create a new list', function (transaction, done) {
-  transaction.request['headers']['Cookie'] = stash.cookie;
+hooks.before('lists > list > Create a new list', (transaction, done) => {
+  transaction.request.headers.Cookie = stash.cookie;
   done();
 });
-hooks.before('items > item > Create a new item for a list', function (transaction, done) {
-  transaction.request['headers']['Cookie'] = stash.cookie;
+hooks.before('items > item > Create a new item for a list', (transaction, done) => {
+  transaction.request.headers.Cookie = stash.cookie;
   transaction.fullPath = `/api/list/${stash.list}/item`;
   transaction.request.uri = `/api/list/${stash.list}/item`;
   done();
 });
-hooks.after('items > item > Create a new item for a list', function (transaction, done) {
+hooks.after('items > item > Create a new item for a list', (transaction, done) => {
   const item = transaction.real.body;
   stash.item = JSON.parse(item).id;
   done();
 });
-hooks.after('lists > list > Create a new list', function (transaction, done) {
+hooks.after('lists > list > Create a new list', (transaction, done) => {
   const list = transaction.real.body;
   stash.list = JSON.parse(list).listId;
   done();
 });
-hooks.before('lists > list/:list > Send back a specific lists that a user has access to', function (transaction, done) {
-  transaction.request['headers']['Cookie'] = stash.cookie;
+hooks.before('lists > list/:list > Send back a specific lists that a user has access to', (transaction, done) => {
+  transaction.request.headers.Cookie = stash.cookie;
   transaction.fullPath = `/api/list/${stash.list}`;
   transaction.request.uri = `/api/list/${stash.list}`;
   done();
 });
-hooks.before('lists > list/:list > Delete a specific list that a user has access to', function (transaction, done) {
-  transaction.request['headers']['Cookie'] = stash.cookie;
+hooks.before('lists > list/:list > Delete a specific list that a user has access to', (transaction, done) => {
+  transaction.request.headers.Cookie = stash.cookie;
   transaction.fullPath = `/api/list/${stash.list}`;
   transaction.request.uri = `/api/list/${stash.list}`;
   done();
 });
-hooks.before('items > items > Send back a collection of all items that a user has access to on a given list', function (transaction, done) {
-  transaction.request['headers']['Cookie'] = stash.cookie;
+hooks.before('items > items > Send back a collection of all items that a user has access to on a given list', (transaction, done) => {
+  transaction.request.headers.Cookie = stash.cookie;
   transaction.fullPath = `/api/list/${stash.list}/items`;
   transaction.request.uri = `/api/list/${stash.list}/items`;
   done();
 });
-hooks.before('items > list/:list/item/:item > Send back a specific item that a user has access to', function (transaction, done) {
-  transaction.request['headers']['Cookie'] = stash.cookie;
+hooks.before('items > list/:list/item/:item > Send back a specific item that a user has access to', (transaction, done) => {
+  transaction.request.headers.Cookie = stash.cookie;
   transaction.fullPath = `/api/list/${stash.list}/item/${stash.item}`;
   transaction.request.uri = `/api/list/${stash.list}/item/${stash.item}`;
   done();
