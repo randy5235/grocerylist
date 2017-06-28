ed7fd5492fc3fef97bc26511317604793f0b8403
Randy Wressell <randy5235@hotmail.com>
2017-06-28 13:14:02 -0700

    Update request logging

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const logger = require('morgan');
 const passport = require('passport');
 const router = require('./routes');
 const session = require('express-session');
+const winston = require('winston');
+
+winston.stream = {
+  write: (message) => {
+    winston.info(message);
+  }
+};
+
 require('./authentication/auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(session(sessionSecret));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
+app.use(logger('combined', { stream: winston.stream }));
 app.use('/api', router);
 app.listen(port);
