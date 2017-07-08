006ec23c9d8de6c9bbf9040c979164b4f2dcf2bc
Randy Wressell <randy5235@hotmail.com>
2017-07-07 22:33:52 -0700

    Add redis store

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const logger = require('morgan');
 const passport = require('passport');
 const router = require('./routes');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redisOptions = require('./config/redis.json');
 const winston = require('winston');
 
 winston.stream = {
@@ -19,7 +21,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // app.use(logger('dev'));
-app.use(session(sessionSecret));
+app.use(session(Object.assign({ store: new RedisStore(redisOptions) }, sessionSecret)));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
